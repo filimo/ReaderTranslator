ffab6ecd7438129260597ef987e74da1f34c8ccb
Viktor Kushnerov <filimo@gmail.com>
2019-09-23 10:25:08 +0300

    Refactoring Published.init.

--- ReaderTranslator/Property Wrappers/Published.swift
+++ ReaderTranslator/Property Wrappers/Published.swift
@@ -1,15 +1,17 @@
 import Foundation
 import Combine
 
-private var cancellables = [String:AnyCancellable]()
+private var cancellableSet: Set<AnyCancellable> = []
 
 extension Published {
     init(wrappedValue defaultValue: Value, key: String) {
         let value = UserDefaults.standard.object(forKey: key) as? Value ?? defaultValue
         self.init(initialValue: value)
-        cancellables[key] = projectedValue.sink { val in
-            UserDefaults.standard.set(val, forKey: key)
-        }
+        projectedValue
+            .sink { val in
+                UserDefaults.standard.set(val, forKey: key)
+            }
+            .store(in: &cancellableSet)
     }
 }
 
@@ -19,9 +21,11 @@ extension Published where Value: RawRepresentable, Value.RawValue == String {
         let value = Value.init(rawValue: savedValue) ?? defaultValue
         
         self.init(initialValue: value)
-        cancellables[key] = projectedValue.sink { val in
-            UserDefaults.standard.set(val.rawValue, forKey: key)
-        }
+        projectedValue
+            .sink { val in
+                UserDefaults.standard.set(val.rawValue, forKey: key)
+            }
+            .store(in: &cancellableSet)
     }
 }
 
