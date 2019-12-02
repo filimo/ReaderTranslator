239a89d19cef40d9fe69e70dcf79721c21266746
Viktor Kushnerov <filimo@gmail.com>
2019-12-02 14:44:19 +0300

    Readme.md

--- ReaderTranslatorPlayer/Components/WKWebRepresenter.swift
+++ ReaderTranslatorPlayer/Components/WKWebRepresenter.swift
@@ -19,6 +19,7 @@ struct WKWebRepresenter: UIViewRepresentable {
         let script = """
             document.addEventListener("DOMContentLoaded", (event) => {
                 document.getElementById('ad_topslot').remove()
+                document.getElementsByClassName('dictionary_intro')[0].remove()
             })
         """
         let userScript = WKUserScript(
