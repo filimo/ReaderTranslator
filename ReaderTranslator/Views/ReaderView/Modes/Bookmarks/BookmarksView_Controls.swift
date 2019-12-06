06821f737207f25038358e68d17b054b27d4cea8
Viktor Kushnerov <filimo@gmail.com>
2019-12-06 21:27:44 +0300

    Bookmarks view #59: Set columns of bookmarks view to 2.

--- ReaderTranslator/Views/ReaderView/Modes/Bookmarks/BookmarksView_Controls.swift
+++ ReaderTranslator/Views/ReaderView/Modes/Bookmarks/BookmarksView_Controls.swift
@@ -15,7 +15,7 @@ struct BookmarksView_Controls: View {
 
     var body: some View {
         HStack {
-            Text("\(store.bookmarks.count)/\(store.bookmarks.checked.count)")
+            Text("\(store.bookmarks.checked.count)/\(store.bookmarks.count)")
             Button(action: {
                 self.showConfirm = true
             }, label: { Text("Remove all") })
