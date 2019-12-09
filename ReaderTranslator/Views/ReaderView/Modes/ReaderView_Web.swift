542a84a8c1909509a891ec52857acfc4d743434d
Viktor Kushnerov <filimo@gmail.com>
2019-12-09 12:37:58 +0300

    Refactoring ReaderView_Web

--- ReaderTranslator/Views/ReaderView/Modes/ReaderView_Web.swift
+++ ReaderTranslator/Views/ReaderView/Modes/ReaderView_Web.swift
@@ -29,9 +29,27 @@ struct ReaderView_Web: View {
                     webView(1)
                     webView(2)
                 }
-                StatusBarView_Tabs()
+                tabsView
             }
         }
+    }
+
+    var tabsView: some View {
+        HStack {
+            Button(
+                action: { self.store.currentTab = 0 },
+                label: { Image.sfSymbol("1.circle\(iconStatus(0))") })
+            Button(
+                action: { self.store.currentTab = 1 },
+                label: { Image.sfSymbol("2.circle\(iconStatus(1))") })
+            Button(
+                action: { self.store.currentTab = 2 },
+                label: { Image.sfSymbol("3.circle\(iconStatus(2))") })
+        }
+    }
+
+    private func iconStatus(_ tab: Int) -> String {
+        self.store.currentTab == tab ? ".fill" : ""
     }
 }
 
