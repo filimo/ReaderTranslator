6f9aaeedf40a2b830cd3d91c8e92e4accc273b6e
Viktor Kushnerov <filimo@gmail.com>
2020-11-11 16:35:10 +0300

    Changed gTranslator zoom for iOS and iPadOS.

--- ReaderTranslatorMobile/Scripts/gtranslator.js
+++ ReaderTranslatorMobile/Scripts/gtranslator.js
@@ -1,13 +1,9 @@
 (function() {
-     const css = " \
-     .reader-mini #gb, .reader-mini .tlid-input, .reader-mini .gp-footer { display: none; } \
-     #source { font-size: 18px!important; } \
-     .translation { font-size: 18px!important; } \
-     "
+//     const css = " textarea, .tlid-translation { font-size: 18px!important; } "
      
-     let style = document.createElement('style')
-     style.innerHTML = css
-     document.head.appendChild(style)
+//     let style = document.createElement('style')
+//     style.innerHTML = css
+//     document.head.appendChild(style)
 
      let mode = localStorage.getItem('readerTranslatorMode') || 'full'
      readerTranslatorMode(mode)
@@ -15,8 +11,10 @@
 
 function readerTranslatorMode(mode) {
 	localStorage.setItem('readerTranslatorMode', mode)
-	let elm = document.querySelector('body')
-	elm.className = elm.className.replace('reader-mini', '')
+//	let elm = document.querySelector('body')
+//	elm.className = elm.className.replace('reader-mini', '')
 
 	if(mode=='mini') { elm.className += " reader-mini" }
+    
+    document.querySelector(`[name="viewport"]`).setAttribute('content', 'width=device-width,minimum-scale=0.8,maximum-scale=0.8')
 }
