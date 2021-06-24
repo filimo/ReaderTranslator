4b440a7072a46d370790592549ce9c02f86e10ae
Viktor Kushnerov <filimo@gmail.com>
2021-06-24 12:39:04 +0300

    Removed adds on Cambridge, Collin's, Longman.

--- ReaderTranslator/Components/ViewRepresentable/Scripts/cambridge.js
+++ ReaderTranslator/Components/ViewRepresentable/Scripts/cambridge.js
@@ -1,3 +1,14 @@
 (function() {
     document.body.style.zoom = 0.8
+    
+    setInterval(() => {
+        let onetrust = document.querySelector('#onetrust-consent-sdk')
+        if (onetrust) onetrust.remove()
+        
+        let topSlot = document.querySelector('.topslot-container')
+        if (topSlot) topSlot.remove()
+        
+        let stickyslot = document.querySelector('#stickyslot_container')
+        if (stickyslot) stickyslot.remove()
+    }, 500)
 })()
--- ReaderTranslator/Components/ViewRepresentable/Scripts/collins.js
+++ ReaderTranslator/Components/ViewRepresentable/Scripts/collins.js
@@ -1,5 +1,15 @@
 (function() {
     document.body.style.zoom = 0.8
-    document.querySelector('#stickyslot_container').remove()
-    document.querySelector('.topslot_container').remove()
+    
+    setInterval(() => {
+        let onetrust = document.querySelector('#onetrust-consent-sdk')
+        if (onetrust) onetrust.remove()
+        
+        let topSlot = document.querySelector('.topslot_container')
+        if (topSlot) topSlot.remove()
+        
+        let stickyslot = document.querySelector('#stickyslot_container')
+        if (stickyslot) stickyslot.remove()
+
+    }, 500)
 })()
